8505da9548d06ad7b9f654e82d842f053c4ec2fe
salauer <stephenalauer@gmail.com>
2017-09-29 19:08:05 -0400

    final code from class on plotting

--- lectures/2017-09-29/plotting-skeleton.R
+++ lectures/2017-09-29/plotting-skeleton.R
@@ -20,3 +20,31 @@ View(ToothGrowth)
 ## Two dimensions are numeric, one is a factor
 ## What kind of plots could we make?
 ## Check out http://www.cookbook-r.com or http://sape.inf.usi.ch/quick-reference/ggplot2/
+
+library(dplyr)
+
+ggplot(data=ToothGrowth) +
+    geom_bar(aes(x=as.factor(dose), y=len), fill="blue", stat="identity")
+
+
+?geom_boxplot
+ggplot(data=ToothGrowth) +
+    geom_boxplot(aes(x=as.factor(dose), y=len), color="blue")
+
+ggplot(data=ToothGrowth) +
+    geom_boxplot(aes(x=as.factor(dose), y=len, color=supp))
+
+?scale_x_discrete
+ggplot(data=ToothGrowth) +
+    geom_boxplot(aes(x=as.factor(dose), y=len, color=supp)) +
+    scale_x_discrete("Dosage") +
+    scale_y_continuous("Tooth length") +
+    scale_color_discrete("Supplement type")
+
+?theme
+ggplot(data=ToothGrowth) +
+    geom_boxplot(aes(x=as.factor(dose), y=len, color=supp)) +
+    scale_x_discrete("Dosage") +
+    scale_y_continuous("Tooth length") +
+    scale_color_discrete("Supplement type") +
+    theme(axis.text.x = element_text(size=20, color="red3"))
